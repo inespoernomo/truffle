18f47a1a36c94f3f4b4a28057d6d00a2eaceba70
Gordon Sun <sunshi_neo@hotmail.com>
2012-09-04 21:20:15 -0700

    Have the edit image activity show the image and caption.

--- CoffeeArrow/src/com/example/coffeearrow/EditImageActivity.java
+++ CoffeeArrow/src/com/example/coffeearrow/EditImageActivity.java
@@ -1,10 +1,14 @@
 package com.example.coffeearrow;
 
+import com.example.coffeearrow.helpers.ImageLoader;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * This is the activity to edit the image caption or delete the image.
@@ -24,6 +28,15 @@ public class EditImageActivity extends Activity {
         String s3url = sourceIntent.getStringExtra("s3url");
         String caption = sourceIntent.getStringExtra("caption");
         Log.i("EditImageActivity", "Got s3url: "+s3url+" . Caption: "+caption);
+        
+        // Setup the image
+        ImageLoader imageLoader = new ImageLoader(this);
+        ImageView imageToEdit = (ImageView)findViewById(R.id.imageToEdit);
+        imageLoader.DisplayImage(s3url, imageToEdit);
+        
+        // Setup the caption
+        EditText captionEdit = (EditText)findViewById(R.id.caption);
+        captionEdit.setText(caption);
     }
 
     @Override
