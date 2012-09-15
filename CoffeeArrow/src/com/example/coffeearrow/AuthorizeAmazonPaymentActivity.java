79bf0040ea75e88bec7cf0486077b602a0dd9a16
Gordon Sun <sunshi_neo@hotmail.com>
2012-09-14 21:15:50 -0700

    Payment done.

--- CoffeeArrow/src/com/example/coffeearrow/AuthorizeAmazonPaymentActivity.java
+++ CoffeeArrow/src/com/example/coffeearrow/AuthorizeAmazonPaymentActivity.java
@@ -1,7 +1,20 @@
 package com.example.coffeearrow;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.apache.http.client.methods.HttpPost;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.coffeearrow.domain.UserProfile;
+import com.example.coffeearrow.server.PostToServerAsyncTask;
+import com.example.coffeearrow.server.PostToServerCallback;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -17,6 +30,9 @@ import android.widget.Toast;
 public class AuthorizeAmazonPaymentActivity extends Activity {
 	
 	private AuthorizeAmazonPaymentActivity mainActivity;
+	
+	public String userId;
+	public String dateId;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,10 +41,10 @@ public class AuthorizeAmazonPaymentActivity extends Activity {
         setContentView(R.layout.activity_authorize_amazon_payment);
         
 		SharedPreferences settings = getSharedPreferences("MyPrefsFile", 0);
-		String userId = settings.getString("userId", null);
+		userId = settings.getString("userId", null);
 		
 		Intent sourceIntent = getIntent();
-		String dateId = sourceIntent.getStringExtra("dateId");
+		dateId = sourceIntent.getStringExtra("dateId");
         
         WebView view = (WebView)findViewById(R.id.webView1);
         view.setWebViewClient(new WebViewClient(){
@@ -38,12 +54,40 @@ public class AuthorizeAmazonPaymentActivity extends Activity {
         		try {
 					URL url = new URL(urlStr);
 					String host = url.getHost();
-					Log.i("Payment", "host is: "+url);
+					Log.i("Payment", "host is: "+host);
 					
 					if(host.equals("truffle.io")) {
 						view.setVisibility(View.GONE);
-						Toast toast = Toast.makeText(mainActivity, "Payment activity taking over. Will post to server with url: "+ urlStr, Toast.LENGTH_LONG);
-						toast.show();
+						HttpPost request = new HttpPost(urlStr);
+						
+						PostToServerCallback callback = new PostToServerCallback() {
+							public void callback(Object objResult) {
+								String status = "failed";
+								if (objResult != null) {
+									// Parse the JSON
+									JSONArray resultArray = (JSONArray) objResult;
+									try {
+										for (int i = 0; i < resultArray.length(); i++) {
+											JSONObject record = resultArray.getJSONObject(i);
+
+											status = record.getString("status");
+										}
+									} catch (JSONException e) {
+										e.printStackTrace();
+									}
+								}
+								Log.i("Payment", "Server got token?: "+status);
+								
+								if(status.equals("OK")) {
+									Intent intent = new Intent(mainActivity, AskThemOut.class);
+									intent.putExtra("userId", mainActivity.userId);
+									intent.putExtra("dateId", mainActivity.dateId);
+									mainActivity.startActivity(intent);
+								}
+							}
+						};
+						PostToServerAsyncTask task = new PostToServerAsyncTask(callback);
+						task.execute(request);
 						return true;
 					}
 					
@@ -56,7 +100,10 @@ public class AuthorizeAmazonPaymentActivity extends Activity {
                 return false;
             }
         });
-        view.loadUrl("http://coffeearrow.com/callPayments?userid="+userId+"&dateId="+dateId);
+        
+        String paymentUrl = "http://coffeearrow.com/callPayments?userId="+userId+"&dateId="+dateId;
+        Log.i("Payment", "Payment url is: "+paymentUrl);
+        view.loadUrl(paymentUrl);
     }
 
     @Override
