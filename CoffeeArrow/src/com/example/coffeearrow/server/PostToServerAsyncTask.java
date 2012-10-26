ad2809dcf25cf6bd866ef1875b7121d1d1579dd3
Gordon Sun <sunshi_neo@hotmail.com>
2012-10-26 00:03:50 -0700

    Change back the server url. Also add status check on the return reuslt.

--- CoffeeArrow/src/com/example/coffeearrow/server/PostToServerAsyncTask.java
+++ CoffeeArrow/src/com/example/coffeearrow/server/PostToServerAsyncTask.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -34,13 +35,17 @@ public final class PostToServerAsyncTask extends AsyncTask<HttpPost, Integer, Ob
 		try {
 			Log.i("SeverInterface", "request url is:"+request.getURI());
 			response = client.execute(request);
-			Log.i("ServerInterface", "response code:"+response.getStatusLine());
-			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-			String json = reader.readLine();
-			System.out.println(json);
-			Log.i("ServerInterface", "json is:"+json);
-			JSONObject obj = new JSONObject(json);
-			finalResult = obj.getJSONArray("results");
+			StatusLine status = response.getStatusLine();
+			Log.i("ServerInterface", "response code:"+status.toString());
+			
+			if (status.getStatusCode() ==200) {
+    			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+    			String json = reader.readLine();
+    			System.out.println(json);
+    			Log.i("ServerInterface", "json is:"+json);
+    			JSONObject obj = new JSONObject(json);
+    			finalResult = obj.getJSONArray("results");
+			}
 		} catch (ClientProtocolException e) {		
 			e.printStackTrace();
 		} catch (IOException e) {
